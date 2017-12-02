d452070ba353d134a11810d812427dfe17f9017e
caleblareau <caleblareau@g.harvard.edu>
2017-12-02 11:19:25 -0500

    v 0.5.1 bump: tf1

--- R/cell_colors.R
+++ R/cell_colors.R
@@ -110,3 +110,61 @@ ejc_color_map <- function(name) {
 return(unname(ejc_color_maps[name]))
 }
 
+#' List of maps between annotations and specified colors
+#' for the TF1 phylogeny analyses.
+#'
+#' Use tf1_color_map to map cell/feature to color hex.
+#'
+#' @export
+tf1_color_maps <- c(
+  "C9" = "#F6313E",
+  "D6" = "#46A040",
+  "G10" = "#0081C9",
+
+  "D2" = "#FFA300", # 21
+  "G11" = "#A65AC2", # 21
+  "B3" = "#FFFF32", # 13
+  "B5" = "#89774A", #5
+  "B11" = "#FF6A80", #5
+  "F4" = "#999999", #5
+  "A9" = "#0DB2AA", #3
+
+  "bulk" = "#001588",
+  "mix"= "#00441B",
+
+  "B9" = "#CDA2DB",
+  "C10" = "#98D9E9",
+  "C4" = "#FF9999",
+  "C7" =  "#FFC966",
+  "D3" = "#A9FBA9"
+)
+
+#' A mapping function to specific colors for the
+#' TF1 clone paper
+#'
+#' This function maps a common feature/cell
+#' type (e.g. "B9") and returns the canonical
+#' hex code associtated with it.
+#'
+#' color_map (no 's') is the function; color_maps
+#' is a named character vector.
+#'
+#' @param name a character (vector) corresponding to a
+#' name with a mappable color
+#' @return a character (vector) of hex codes
+#' @keywords colors
+#' @examples
+#' # See possible values
+#' names(tf1_color_maps)
+#'
+#' # Get hex codes
+#' tf1_color_map("B9")
+#' tf1_color_map(c("C9", "D6", "G10"))
+#'
+#' @export
+tf1_color_map <- function(name) {
+  stopifnot(all(name %in% names(tf1_color_maps)))
+return(unname(tf1_color_maps[name]))
+}
+
+
